2a05bf9d0b40faa2b50ed03b6f2eed0e6bfe60a7
VVladLytvyn <1997vlad1997bob@gmail.com>
2024-03-24 19:36:04 +0200

    second commit, adding loop for questions and conditions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const countOfFilms = prompt('сколько фильмов вы посмотрели?', '');
+const countOfFilms = +prompt('сколько фильмов вы посмотрели?', '');
 
 const personalMovieDB = {
 	count: countOfFilms,
@@ -9,13 +9,43 @@ const personalMovieDB = {
 	privat: false,
 };
 
-const lastWachedMovie = prompt('один из последних просмотренных фильмов?', ''),
-				scoreFilm = prompt('во сколько оцените этот фильм?', ''),
-				lastWachedMovie_1 = prompt('один из последних просмотренных фильмов?', ''),
-				scoreFilm_1	= prompt('а этот во сколько оцените?', '')	 
+for (let i = 0; i < 2; i++){
+	const a = prompt('один из последних просмотренных фильмов?', '');
+	const b = prompt('во сколько оцените этот фильм?', +'');
+	
+	if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done ');
+	} else {
+		console.log('error');
+		i--;
+	}
+}  
+
+// let i = 0;
+// while (i < 2) {
+// 	const a = prompt('the last watched movie?', '');
+// 	const b = prompt('put the score of that movie', +''); 
+	
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('done');
+// 	} else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// 	i++;
+// }
 
 
-personalMovieDB.movies[lastWachedMovie] = scoreFilm;
-personalMovieDB.movies[lastWachedMovie_1] = scoreFilm_1;			
+if (personalMovieDB.count < 10) {
+	console.log('you have watched very few movies');
+} else if (personalMovieDB >= 10 && personalMovieDB.count <= 30) {
+	console.log('you like to spent time in front of tv');
+} else if (personalMovieDB.count > 30) {
+	console.log('you are a movieLover')
+} else {
+	console.log('error');
+}
 
 console.log(personalMovieDB);
